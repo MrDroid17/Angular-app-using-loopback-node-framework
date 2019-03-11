65ef8c057da5b2c00fcdf4e1be132cf7f13a68db
Sobhit Kumar <sobhit2010@gmail.com>
2019-03-11 09:48:51 +0530

    login form Ui and api integration

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Http, Headers } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { headersToString } from 'selenium-webdriver/http';
+import { environment } from '../../environments/environment';
+// import { HEADERS } from '../utils/Constant'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  token: any;
+  username: string;
 
-  constructor() { }
+
+  signupForm: FormGroup = new FormGroup({
+    id: new FormControl(null),
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required]),
+    confirm_password: new FormControl('', [Validators.required]),
+  });
+
+  loginForm: FormGroup = new FormGroup({
+    id: new FormControl(null),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  /***
+   * urls
+   */
+  LOGIN_URL: string = environment.serverUrl + '/api/Users/login';
+
+
+  constructor(
+    private http: Http
+  ) { }
+
+
+  getHeaders(): any {
+    // this.loadToken();
+    const headers = new Headers();
+    // headers.append(HEADERS.AUTHENTICATION_TOKEN_KEY, this.token);
+    // headers.append(HEADERS.STAFF_USER_ID_KEY, this.username);
+    headers.append('Content-Type', 'application/json');
+    return headers;
+  }
+
+  // authenticate
+  userLogin(user) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(this.LOGIN_URL, user, { headers: headers }).pipe(map(res => res.json()));
+  }
+
+
 }
